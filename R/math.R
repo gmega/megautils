c7ff521eb9d0a593548732f79ccd3f6fccc06106
gmega <giuliano.mega@gmail.com>
2020-06-04 18:34:09 -0300

    add roll_stat and math tests

--- R/math.R
+++ R/math.R
@@ -6,3 +6,23 @@ log1pneg <- function(x) {
   x[!pos & not_na] <- -log(abs(x[!pos & not_na] - 1))
   x
 }
+
+#' Computes rolling statistics
+#' 
+#' Computes a rolling statistic `stat` over windows of size `n` taken from
+#' vector `x`.
+#' 
+#' @param x a vector
+#' @param n a window size 
+#' @stat stat an aggregation function which returns a value for each window
+#' 
+#' @examples 
+#' 
+#'   x <- c(1, 0, 1, 1, 0, 1, 0, 0, 0)
+#'   # returns 2 2 2 2 1 1 0
+#'   roll_stat(x, n = 3, sum)
+#' 
+#' @export
+roll_stat <- function(x, n, stat) {
+  sapply(1:(length(x) - n + 1), function(i) stat(x[i:(i + n - 1)]))
+}
---
+++ tests/testthat/test_math.R
@@ -0,0 +1,17 @@
+context('math')
+
+test_that('roll_stat computes rolling stats', {
+  x <- c(TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE)
+  y <- c(2, 1, 0, 1, 1, 2)
+  
+  expect_equal(roll_stat(x, 3, sum), y)
+})
+
+
+test_that('log1pneg computes scale for negative values and NA', {
+  part <- c(20, 10, 5, 1, 0)
+  x <- c(part, NA, -rev(part))
+  y <- c(log1p(part), NA, -log1p(rev(part)))
+  
+  expect_equal(log1pneg(x), y)
+})
